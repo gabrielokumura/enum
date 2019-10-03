9cbab9d56ac8dfe7f259d98529c49a08d9917d8b
Gabrie Okumura <gabrielokumura2017@gmail.com>
2019-10-03 15:16:51 -0300

    enumeracoes

---
+++ exeEnum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using exeEnum.Entities;
+using exeEnum.Entities.Enums;
+
+namespace exeEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
